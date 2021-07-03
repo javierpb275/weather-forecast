b9bc84b205d45521cea5d154c0c5aa1fa6fa4ea2
javierpb275 <javierpb275@gmail.com>
2021-07-03 12:57:40 +0200

    used app.listen() to run the app on the port 3000. Used app.get() to set a route where we send a response.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
 const express = require('express');
 
 const app = express();
+
+app.listen(3000, () => console.log('Server is up on port 3000.'));
+
+app.get('', (req, res) => {
+    res.send('Hello Express!');
+});
+
