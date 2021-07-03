1e6d1672261dd76026b13400350a717738384eed
javierpb275 <javierpb275@gmail.com>
2021-07-03 14:01:14 +0200

    removed '' /help and /about routes

--- src/app.js
+++ src/app.js
@@ -6,29 +6,6 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(3000, () => console.log('Server is up on port 3000.'));
-
-app.get('', (req, res) => {
-    res.send('<h1>WEATHER</h1>');
-});
-
-app.get('/help', (req, res) => {
-    res.send([
-        {
-        name: 'Pepe',
-        age: 40
-        },
-        {
-        name: 'Paco',
-        age: 37
-        }
-    ]);
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>ABOUT</h1>');
-});
-
 app.get('/weather', (req, res) => {
     res.send([
         {
@@ -41,3 +18,5 @@ app.get('/weather', (req, res) => {
         }
     ]);
 })
+
+app.listen(3000, () => console.log('Server is up on port 3000.'));
