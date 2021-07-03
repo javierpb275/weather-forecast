011e31dde504b03bb32b03a7b4c20dceecbf882f
javierpb275 <javierpb275@gmail.com>
2021-07-03 17:29:24 +0200

    set up partialsPath using hbs in app.js. created partials folder where we put the partials (small pieces of a page like header). created header partial inside. also moved files of templtes to new folder called views inside.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 //setup handlebars engine and views location:
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 //setup static directory to serve
 app.use(express.static(publicDirectoryPath));
