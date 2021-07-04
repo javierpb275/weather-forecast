18ff7d049d69d7b87dd60da827a698eaecf95b6b
javierpb275 <javierpb275@gmail.com>
2021-07-04 13:03:57 +0200

    created 404 page and routes for that page

--- src/app.js
+++ src/app.js
@@ -52,4 +52,20 @@ app.get('/weather', (req, res) => {
     ]);
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Javier',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Javier',
+        errorMessage: 'Page not found.'
+    })
+})
+
 app.listen(3000, () => console.log('Server is up on port 3000.'));
