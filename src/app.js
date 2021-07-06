4147090b30495322b13c15077c60cdb2cdd3441d
javierpb275 <javierpb275@gmail.com>
2021-07-06 18:15:01 +0200

    the query string: check if we get a request and if not we send an error. we do return because we can only do res.send() once, otherwise we get an error.

--- src/app.js
+++ src/app.js
@@ -53,6 +53,11 @@ app.get('/weather', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
+    if(!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
     console.log(req.query.search);
     res.send({
         products: []
