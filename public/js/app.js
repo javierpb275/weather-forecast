fec96858e43ac2865284215af66b906b4434d633
javierpb275 <javierpb275@gmail.com>
2021-07-07 18:23:39 +0200

    HTTP request with Fetch fetch is provided by the browser so we can only  use it in the client side. We cannot use it in a node server.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
 console.log('Client side javascript file is loaded!')
+
+/*fetch('https://puzzle.mead.io/puzzle').then(response => {
+    response.json().then(data => {
+        console.log(data);
+    })
+})*/
+
+fetch('http://localhost:3000/weather?address=Los Angeles').then(response => {
+    response.json().then(data => {
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log(data);
+        }
+    })
+})
