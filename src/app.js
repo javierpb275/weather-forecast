8f8ee7b9d94f7a3b676745c945ab6caf1cb03347
javierpb275 <javierpb275@gmail.com>
2021-07-09 17:09:08 +0200

    formated code with prettier

--- src/app.js
+++ src/app.js
@@ -1,106 +1,103 @@
 //utils:
-const geocode = require('./utils/geocode');
-const forecast = require('./utils/forecast');
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 //packages
-const express = require('express');
-const path = require('path');
-const hbs = require('hbs');
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
 
 const app = express();
 
 //Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //setup handlebars engine and views location:
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
 //setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Javier'
-    });
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Javier",
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Javier'
-    });
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Javier",
+  });
+});
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'Help Page',
-        message: 'This is the help page',
-        name: 'Javier'
-    });
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    message: "This is the help page",
+    name: "Javier",
+  });
+});
 
-app.get('/weather', (req, res) => {
-    const {address} = req.query
-    if (!address) {
+app.get("/weather", (req, res) => {
+  const { address } = req.query;
+  if (!address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  } else {
+    geocode(address, (error, { place, location } = {}) => {
+      if (error) {
         return res.send({
-            error: 'You must provide an address'
-        })
-    } else {
-        geocode(address, (error, {place, location} = {}) => {
-            if(error) {
-                return res.send({
-                    error
-                })
-            }
-            forecast(place, (error, forecastData) => {
-                if(error) {
-                    return res.send({
-                        error
-                    })
-                }
-                res.send(
-                    {
-                    location,
-                    forecast: forecastData
-                    }
-                );
-            })
-    
-        })
-    }
-})
+          error,
+        });
+      }
+      forecast(place, (error, forecastData) => {
+        if (error) {
+          return res.send({
+            error,
+          });
+        }
+        res.send({
+          location,
+          forecast: forecastData,
+        });
+      });
+    });
+  }
+});
 
-app.get('/products', (req, res) => {
-    if(!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
-    }
-    console.log(req.query.search);
-    res.send({
-        products: []
-    })
-})
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
 
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Javier',
-        errorMessage: 'Help article not found.'
-    })
-})
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Javier",
+    errorMessage: "Help article not found.",
+  });
+});
 
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Javier',
-        errorMessage: 'Page not found.'
-    })
-})
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Javier",
+    errorMessage: "Page not found.",
+  });
+});
 
-app.listen(3000, () => console.log('Server is up on port 3000.'));
+app.listen(3000, () => console.log("Server is up on port 3000."));
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,18 +1,27 @@
 //packages:
-const request = require('request');
+const request = require("request");
 
 const forecast = (address, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=5287c1ed8a65a3139af80d11998a60ca&query=' + encodeURIComponent(address);
-    request({url, json: true}, (err, {body}) => {
-        const {current, error} = body
-        if(err) {
-            callback('Unable to connect to weather service!',undefined)
-        } else if(error) {
-            callback('Unable to find location',undefined)
-        } else {
-            callback(undefined, 'It is ' + current.temperature + ' degrees out. And there is a ' + current.precip + '% chance of rain.')
-        }
-    })
-}
+  const url =
+    "http://api.weatherstack.com/current?access_key=5287c1ed8a65a3139af80d11998a60ca&query=" +
+    encodeURIComponent(address);
+  request({ url, json: true }, (err, { body }) => {
+    const { current, error } = body;
+    if (err) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        "It is " +
+          current.temperature +
+          " degrees out. And there is a " +
+          current.precip +
+          "% chance of rain."
+      );
+    }
+  });
+};
 
 module.exports = forecast;
